ff0313557b89e7ceff888f7e64fcf50f5680f1f5
adri <pikaljovadri@gmail.com>
2025-03-19 09:59:36 +0200

    Update Kasutajate_funktsioonid.py

--- Kasutajate_funktsioonid.py
+++ Kasutajate_funktsioonid.py
@@ -1,4 +1,32 @@
 from mooduldef import *
+
+
+#lihtarvud
+n=int(input("sisesta arv: "))
+v=is_prime(n)
+print(v)
+
+#kuupaevad
+kuu=int(input("sisesta kuu number: "))
+paev=int(input("sisesta paev: "))
+v=date(kuu, paev)
+print(v)
+
+
+#bank
+a=int(input("sisesta raha: "))
+years=int(input("sisesta aastad: "))
+v=bank(a, years)
+print(v)
+
+
+#XOR krüpteerimine
+text=input("sisesta tekst: ")
+key=input("sisesta võti: ")
+v=XOR_cipher(text, key)
+print(v)
+
+
 #Простейшие арифметические операции 
 a=float(input("sisesta arv 1: "))
 b=float(input("sisesta arv 2: "))
@@ -22,5 +50,6 @@ print(v)
 
 #aastaajad 
 k=int(input("sisesta kuu number: "))
-v=season(k)
-print(v)
+v= season(k)
+print(v)    
+
